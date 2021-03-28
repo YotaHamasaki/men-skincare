1b20436e1185c1b88f6facb27146c1998ba4f0a1
yota.hamasaki <journey231517@yahoo.co.jp>
2021-03-28 13:04:43 +0000

    change top-image

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
     mount_uploader :image, ImageUploader
     has_many :posts, dependent: :destroy
+    
+    validates :item, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,6 @@ class Post < ApplicationRecord
     less_than_or_equal_to: 5,
     greater_than_or_equal_to: 1
   }, presence: true
+    validates :title, presence: true
+    validates :content, presence: true
 end
