2385ce945845553c4385697bac1d3d156357b936
yota.hamasaki <journey231517@yahoo.co.jp>
2021-03-28 13:08:48 +0000

    change validate

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,6 @@ class Item < ApplicationRecord
     mount_uploader :image, ImageUploader
     has_many :posts, dependent: :destroy
     
-    validates :item, presence: true
+    validates :name, presence: true
+    validates :image, presence: true
 end
