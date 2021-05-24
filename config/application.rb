57ad052b0fdd433b51c7312e56ac39e52eeab2f8
Ubuntu <ubuntu@ip-172-31-4-132.ap-northeast-1.compute.internal>
2021-05-24 14:05:31 +0000

    fix config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,31 +1,31 @@
-# require_relative "boot"
+require_relative "boot"
 
-# require "rails"
+require "rails"
 # # Pick the frameworks you want:
-# require "active_model/railtie"
-# require "active_job/railtie"
-# require "active_record/railtie"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
 # # require "active_storage/engine"
-# require "action_controller/railtie"
-# require "action_mailer/railtie"
-# # require "action_mailbox/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+#require "action_mailbox/engine"
 # # require "action_text/engine"
-# require "action_view/railtie"
-# require "action_cable/engine"
-# require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # # Require the gems listed in Gemfile, including any gems
 # # you've limited to :test, :development, or :production.
-# Bundler.require(*Rails.groups)
+Bundler.require(*Rails.groups)
 
 
 
-# module SkincareApp
-#   class Application < Rails::Application
+module SkincareApp
+ class Application < Rails::Application
 #     # Initialize configuration defaults for originally generated Rails version.
-#     config.load_defaults 5.2
-#     config.i18n.default_locale = :ja
+config.load_defaults 5.2
+config.i18n.default_locale = :ja
 #     # Configuration for the application, engines, and railties goes here.
 #     #
 #     # These settings can be overridden in specific environments using the files
@@ -33,5 +33,5 @@
 #     #
 #     # config.time_zone = "Central Time (US & Canada)"
 #     # config.eager_load_paths << Rails.root.join("extras")
-#   end
-# end
+ end
+end
